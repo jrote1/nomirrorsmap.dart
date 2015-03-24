42ef8614a962d2bb64a00552528643a214997381
Adeel <adeel.mahmood@liquidthinking.co.uk>
2015-03-24 12:10:37 +0000

    added missing tests
    added missing test for $ref issue in NewtonSoft converter

--- test/tests.dart
+++ test/tests.dart
@@ -286,10 +286,22 @@ main( )
 			var simpleType = result as SimpleTypeUsingDollarRef;
 			expect(result.name, result.people[1].name);
 			expect(result.people[1].name, "Test User");
+		});
+
+		test("can serialize object with no properties", ()
+		{
+			var json = '''[{"\$id":"994910500","\$type":"nomirrorsmap.tests.TypeWithNoProperties"},{"\$ref":"994910500"}]''';
+
+			var result = new NoMirrorsMap( ).convert( json, new NewtonSoftJsonConverter(), new ClassConverter(startType: const TypeOf<List<TypeWithNoProperties>>().type) );
 
 
 		});
 	});
+
+}
+
+class TypeWithNoProperties
+{
 
 }
 
@@ -410,4 +422,10 @@ class AssociationLevel {
 
 class ClassWithDateTime{
 	DateTime time;
+}
+
+class TypeOf<T> {
+	Type get type => T;
+
+	const TypeOf();
 }
