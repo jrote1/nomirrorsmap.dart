c7813a0af68dee922ea34947cfb19549e2e7edb4
Jake Rote <jrote1@me.com>
2015-09-28 14:32:47 +0100

    fix(Travis): Remove building of build.dart

--- travis.sh
+++ travis.sh
@@ -8,9 +8,6 @@ $(dirname -- "$0")/ensure_dartfmt.sh
 # Run the tests.
 pub run unittest
 
-# Run the build.dart file - just to make sure it works
-dart --checked build.dart
-
 # Install dart_coveralls; gather and send coverage data.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
   pub global activate dart_coveralls
